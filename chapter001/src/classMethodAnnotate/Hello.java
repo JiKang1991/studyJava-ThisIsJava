abfeadb9745901d98e664af09ba62f133d5e2dd8
jikang1991 <weaktothewall@gmail.com>
2021-01-09 01:45:43 +0900

    study Java chapter001(1.2.2 ~ 1.5)

---
+++ chapter001/src/classMethodAnnotate/Hello.java
@@ -0,0 +1,14 @@
+package classMethodAnnotate;
+/*
+ 	작성자 : 팽수
+ 	작성일 : 2020. 12. 32
+ */
+public class Hello { // 'Hello' : 클래스 이름
+	// 클래스 블록
+	// 프로그램 실행 진입점 main()
+	public static void main(String[] args) { // 'main' : 메소드 이름
+		// 메소드 블록
+		// 콘솔에 출력하는 실행문
+		System.out.println("Hello, welcome to the java world!");
+	} // end of main
+} // end of class
