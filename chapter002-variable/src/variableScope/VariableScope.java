97dcd36ca6823442b8e89aa4d42ebabef0a50070
jikang1991 <weaktothewall@gmail.com>
2021-01-09 22:37:22 +0900

    study Java chapter002(2.1)
    chapter002 - variable and type
    2.1 variable
     2.1.1 What is variable?
     2.1.2 declaration of variable
     2.1.3 use of variable
     2.1.4 scope of variable

---
+++ chapter002-variable/src/variableScope/VariableScope.java
@@ -0,0 +1,14 @@
+package variableScope;
+
+public class VariableScope {
+	public static void main(String[] args) {
+		/*
+			int v1 = 15;
+			if(v1 > 10){
+				int v2 = v1 - 10;
+			}
+			int v3 = v1 + v2 + 5;	// v2 변수를 사용할 수 없기 때문에 컴파일 에러가 발생
+									// v2 cannot be resolved to a variable
+		 */
+	}
+}
